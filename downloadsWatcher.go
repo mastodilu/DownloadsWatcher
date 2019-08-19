b526e8f1527f30dea08619ba556079b51eb2e3a1
mastodilu <matteo.dilucchio@edu.unito.it>
2019-08-19 13:04:51 +0200

    func MoveFile

--- downloadsWatcher.go
+++ downloadsWatcher.go
@@ -3,9 +3,55 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/fsnotify/fsnotify"
+)
+
+// MoveFile moves src file into a destination folder
+func MoveFile(src string) error {
+	// open file to copy
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	lastIndex := strings.LastIndexAny(src, "/")
+	if lastIndex == -1 {
+		return fmt.Errorf("last index of character '/' is -1 in source file")
+	}
+	filename := src[lastIndex:len(src)]
+
+	// create new destination file into destination folder
+	dstFile, err := os.OpenFile(filepath.Join(baseDst, filename), os.O_CREATE|os.O_WRONLY|os.O_EXCL, os.FileMode(0666)) // TODO change baseDst based on file format
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	// write into destination file
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return err
+	}
+
+	// delete original file
+	err = os.Remove(src)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Moved", src)
+	return nil // OK
+}
+
+const (
+	baseDst string = "/home/mastodilu/PROVA"
 )
 
 func main() {
@@ -24,13 +70,16 @@ func main() {
 			// watch for events
 			case event := <-watcher.Events:
 				if event.Op&fsnotify.Chmod == fsnotify.Chmod {
-					// do something
+					err := MoveFile(event.Name)
+					if err != nil {
+						log.Fatalln(err)
+					}
 				}
 
-				fmt.Printf("%v %v\n",
-					event.Op.String(), // operation name
-					event.Name,        // file name
-				)
+				// fmt.Printf("%v %v\n",
+				// 	event.Op.String(), // operation name
+				// 	event.Name,        // file name
+				// )
 
 			// watch for errors
 			case err := <-watcher.Errors:
