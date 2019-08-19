4104ce690c6dac566273c452344cba0e378b1f44
mastodilu <matteo.dilucchio@edu.unito.it>
2019-08-19 19:21:01 +0200

    selected destination folder based on file format

--- downloadsWatcher.go
+++ downloadsWatcher.go
@@ -43,8 +43,45 @@ func CreteFolder(path string, perms uint32) {
 	}
 }
 
+// selectDestFolder returns the right folder path based on the file extension passed as input
+func selectDestFolder(sourceExtension string) string {
+	sourceExtension = strings.TrimPrefix(sourceExtension, ".") // trim the . from the file extension
+	for _, ext := range imageExtensions {
+		if ext == sourceExtension {
+			return filepath.Join(baseDst, "Pictures")
+		}
+	}
+	for _, ext := range videoExtensions {
+		if ext == sourceExtension {
+			return filepath.Join(baseDst, "Videos")
+		}
+	}
+	for _, ext := range audioExtensions {
+		if ext == sourceExtension {
+			return filepath.Join(baseDst, "Music")
+		}
+	}
+	for _, ext := range documentExtensions {
+		if ext == sourceExtension {
+			return filepath.Join(baseDst, "Documents")
+		}
+	}
+	for _, ext := range executableExtensions {
+		if ext == sourceExtension {
+			return filepath.Join(baseDst, "Downloads", "Eseguibili")
+		}
+	}
+	for _, ext := range archivesExtensions {
+		if ext == sourceExtension {
+			return filepath.Join(baseDst, "Downloads", "Archivi")
+		}
+	}
+	return filepath.Join(baseDst, "Downloads", "Altro")
+}
+
 // MoveFile moves src file into a destination folder
 func MoveFile(src string) error {
+
 	// open file to copy
 	srcFile, err := os.Open(src)
 	if err != nil {
@@ -61,10 +98,11 @@ func MoveFile(src string) error {
 	// obtain file extension
 	fileextension := FileExtension(filename)
 
-	fmt.Println("filename", filename, "extension", fileextension)
+	// create destination path string
+	destPath := filepath.Join(selectDestFolder(fileextension), filename)
 
 	// create new destination file into destination folder
-	dstFile, err := os.OpenFile(filepath.Join(baseDst, filename), os.O_CREATE|os.O_WRONLY|os.O_EXCL, os.FileMode(0666)) // TODO change baseDst based on file format
+	dstFile, err := os.OpenFile(destPath, os.O_CREATE|os.O_WRONLY|os.O_EXCL, os.FileMode(0666)) // TODO change baseDst based on file format
 	if err != nil {
 		return err
 	}
@@ -82,22 +120,21 @@ func MoveFile(src string) error {
 		return err
 	}
 
-	fmt.Println("Moved", src)
+	fmt.Println("Moved", src, "into", destPath)
 	return nil // OK
 }
 
 const (
-	baseDst string = "/home/mastodilu/PROVA"
+	baseDst string = "/home/mastodilu"
 )
 
 var (
 	imageExtensions      = []string{"jpeg", "jpg", "gif", "png", "bmp", "raw", "tiff", "psd", "cr2"}
 	videoExtensions      = []string{"avi", "flv", "wmv", "mov", "mp4"}
 	audioExtensions      = []string{"pcm", "wav", "aiff", "mp3", "aac", "ogg", "wma", "flac", "alac", "wma", "m3u"}
-	documentsExtensions  = []string{"doc", "docx", "log", "odt", "rtf", "tex", "txt", "csv", "pps", "ppt", "pptx", "xml", "xls", "xlsx", "db", "dbf", "sql"}
+	documentExtensions   = []string{"pdf", "doc", "docx", "log", "odt", "rtf", "tex", "txt", "csv", "pps", "ppt", "pptx", "xml", "xls", "xlsx", "db", "dbf", "sql"}
 	executableExtensions = []string{"apk", "app", "bat", "com", "exe", "jar", "wsf"}
-	// webFilesExtensions = []string{"asp", "aspx", "cer", "css", "htm", "html", "js", "jsp", "php", "xhtml"}
-	archivesExtensions = []string{"7z", "deb", "tar.gz", "zip", "zipx"}
+	archivesExtensions   = []string{"7z", "deb", "tar.gz", "zip", "zipx"}
 )
 
 func main() {
@@ -121,17 +158,13 @@ func main() {
 			select {
 			// watch for events
 			case event := <-watcher.Events:
+
+				// move file if chmod event occurs
 				if event.Op&fsnotify.Chmod == fsnotify.Chmod {
-					err := MoveFile(event.Name)
-					if err != nil {
-						log.Fatalln(err)
+					if err := MoveFile(event.Name); err != nil {
+						log.Println(err)
 					}
 				}
-
-				// fmt.Printf("%v %v\n",
-				// 	event.Op.String(), // operation name
-				// 	event.Name,        // file name
-				// )
 
 			// watch for errors
 			case err := <-watcher.Errors:
